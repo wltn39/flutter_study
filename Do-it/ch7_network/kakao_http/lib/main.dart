f3c8be3b413aa3fdd871720093c9319fff06c2e2
gino <wltn39@gmail.com>
2023-10-10 22:57:42 +0900

    1010 http #01

---
+++ Do-it/ch7_network/kakao_http/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HttpApp(),
+    );
+  }
+}
+
+class HttpApp extends StatefulWidget {
+  const HttpApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _HttpApp();
+}
+
+class _HttpApp extends State<HttpApp> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Http Example'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('$result'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          getJSONData();
+        },
+        child: Icon(Icons.file_download),
+      ),
+    );
+  }
+
+  Future<String> getJSONData() async {
+    var url = 'https://dapi.kakao.com/v3/search/book?target=title&query-doit';
+    var response = await http.get(Uri.parse(url),
+        headers: {"Authorization": "KakaoAK 52c5d02d9d768250058c3827facddf57"});
+    print(response.body);
+    return "Successfull";
+  }
+}
